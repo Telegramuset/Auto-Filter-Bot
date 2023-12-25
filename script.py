d1b312bc62e3d74b1a5b80820ba573e157421a30
Telegramuset <152580875+Telegramuset@users.noreply.github.com>
2023-12-25 19:02:40 +0530

    script.py

--- script.py
+++ script.py
@@ -29,13 +29,11 @@ Bot will automatically search for your file and give links to that!
 <b>© @TroJanzHEX</b>"""
 
 
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : MAKIMA </b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @DP_CINEMA_CHANNEL
 
 ⭕️<b>Language :</b> <code>Python3</code>
-
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
 ⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
 
